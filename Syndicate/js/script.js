3aef377ab0701a602c99c98e5841ad0e1cf84ee0
Elkhan <98739225+Elkhan2003@users.noreply.github.com>
2022-04-01 19:34:41 +0600

    Blurry

---
+++ Syndicate/js/script.js
@@ -0,0 +1,19 @@
+let loadText = document.querySelector(".loading_text");
+let bg = document.querySelector(".bg");
+let load = 0;
+let int = setInterval(blurring, 40)
+
+function blurring() {
+    load++
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loadText.innerHTML = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 30, 0)
+    bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
+
+    console.log(load)
+}
+const scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
